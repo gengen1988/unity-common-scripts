a8ffd9f32849b1238df351191215b6989c193d83
Gen <gerald_721@hotmail.com>
2023-11-03 17:44:38 +0800

    add set dirty tool

--- Editor/MyProjectTools.cs
+++ Editor/MyProjectTools.cs
@@ -6,42 +6,57 @@ using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+public static class UnityEditorTools
+{
+    [MenuItem("Assets / Tools / Set Selection Dirty")]
+    [MenuItem("GameObject / Tools / Set Selection Dirty")]
+    private static void SetSelectionDirty()
+    {
+        foreach (var go in Selection.objects)
+        {
+            EditorUtility.SetDirty(go);
+        }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
+
 public static class MyProjectTools
 {
     static AddRequest request;
-    
+
     [MenuItem("Tools / My Project / Init HDR Project")]
     static void InitHDRProject()
     {
         // tmp
         TMP_PackageUtilities.ImportProjectResourcesMenu();
-            
+
         // linear for hdr
         PlayerSettings.colorSpace = ColorSpace.Linear;
-        
+
         // disable webgl compression - let nginx handle compression
         PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
-        
+
         // disable auto graphic api for linear color in webgl
         PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.WebGL, false);
-        PlayerSettings.SetGraphicsAPIs(BuildTarget.WebGL, new []{ GraphicsDeviceType.OpenGLES3});
-        
+        PlayerSettings.SetGraphicsAPIs(BuildTarget.WebGL, new[] { GraphicsDeviceType.OpenGLES3 });
+
         // enable hdr for glow
         TierSettings tierSettings;
-        
+
         // standalone
         tierSettings = EditorGraphicsSettings.GetTierSettings(BuildTargetGroup.Standalone, GraphicsTier.Tier3);
         tierSettings.hdr = true;
         EditorGraphicsSettings.SetTierSettings(BuildTargetGroup.Standalone, GraphicsTier.Tier3, tierSettings);
-        
+
         // webgl
         tierSettings = EditorGraphicsSettings.GetTierSettings(BuildTargetGroup.WebGL, GraphicsTier.Tier3);
         tierSettings.hdr = true;
         EditorGraphicsSettings.SetTierSettings(BuildTargetGroup.WebGL, GraphicsTier.Tier3, tierSettings);
-        
+
         // remove bloats
         Client.Remove("com.unity.collab-proxy");
-        
+
         // add post processing package
         request = Client.Add("com.unity.postprocessing");
         EditorApplication.update += Progress;
@@ -53,12 +68,12 @@ public static class MyProjectTools
         {
             switch (request.Status)
             {
-            case StatusCode.Success:
-                Debug.Log("add post processing package success");
-                break;
-            case StatusCode.Failure:
-                Debug.LogError($"add post processing package failure: {request.Error.message}");
-                break;
+                case StatusCode.Success:
+                    Debug.Log("add post processing package success");
+                    break;
+                case StatusCode.Failure:
+                    Debug.LogError($"add post processing package failure: {request.Error.message}");
+                    break;
             }
             EditorApplication.update -= Progress;
         }
